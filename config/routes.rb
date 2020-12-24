6b48a920cf345f58ed337a11e53cc4da5c216181
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2020-12-24 21:53:55 +0900

    プロフィール画像機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users
   resources :iclones do
     collection do
       post :confirm
