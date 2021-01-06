9a19bd702a29ad793708af94fea0d158bf4b9792
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-01-06 11:50:07 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'iclones#index'
   resources :favorites
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
@@ -8,5 +9,4 @@ Rails.application.routes.draw do
     end
   end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  root 'iclones#index'
 end
